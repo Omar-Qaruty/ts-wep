d8eb003113e765283caf62581a4e0c607a5a3f21
ward-qaruty <ward.muse.dev@gmai.com>
2022-03-07 22:32:22 +0300

    extracting a Model class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,4 @@
-import { Attrubutes } from "./Attributes";
-import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
-import axios, { AxiosResponse, AxiosPromise } from "axios";
+import { Model } from "./Model";
 
 export interface UserProps {
   //    the "?" means that the props are optional , you don't need to pass all the props in the new object.
@@ -11,47 +8,4 @@ export interface UserProps {
 }
 const rootUrl = "http://localhost:3000/users";
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attrubutes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attrubutes<UserProps>(attrs);
-  }
-
-  get onEvent() {
-    //   Not calling a function , just returning a refrence to events.onEvent method
-    return this.events.onEvent;
-  }
-  get triggerEvent() {
-    return this.events.triggerEvent;
-  }
-  get get() {
-    return this.attributes.get;
-  }
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.triggerEvent("change");
-  }
-  fetch(): void {
-    const id = this.attributes.get("id");
-    if (typeof id !== "number") {
-      throw new Error("Cannot fetch without ID");
-    }
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.triggerEvent("save");
-      })
-      .catch(() => {
-        this.triggerEvent("Error");
-      });
-  }
-}
+export class User extends Model<UserProps> {}
