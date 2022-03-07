335d3f298908fac2d717943b658f9d14dd04b69d
ward-qaruty <ward.muse.dev@gmai.com>
2022-03-07 22:41:20 +0300

    User refactor

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import { Attrubutes } from "./Attributes";
 import { Model } from "./Model";
+import { AbiSync } from "./ApiSync";
+import { Eventing } from "./Eventing";
 
 export interface UserProps {
   //    the "?" means that the props are optional , you don't need to pass all the props in the new object.
@@ -8,4 +11,12 @@ export interface UserProps {
 }
 const rootUrl = "http://localhost:3000/users";
 
-export class User extends Model<UserProps> {}
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attrubutes<UserProps>(attrs),
+      new Eventing(),
+      new AbiSync<UserProps>(rootUrl)
+    );
+  }
+}
